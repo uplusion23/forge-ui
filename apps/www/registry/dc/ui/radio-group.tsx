f60cb37d2282b9dc69111f6f118bb81f3f2be5ee
uplusion23 <uplusion23@gmail.com>
2024-12-03 10:35:05 -0600

    feat: animate dc radio group selections

--- apps/www/registry/dc/ui/radio-group.tsx
+++ apps/www/registry/dc/ui/radio-group.tsx
@@ -28,13 +28,13 @@ const RadioGroupItem = React.forwardRef<
     <RadioGroupPrimitive.Item
       ref={ref}
       className={cn(
-        "aspect-square h-4 w-4 rounded-full border border-primary text-primary shadow focus:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50",
+        'aspect-square h-4 w-4 rounded-full border border-primary bg-primary/0 text-primary-foreground shadow transition-colors duration-200 focus:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:cursor-not-allowed disabled:opacity-50 [&[data-state="checked"]]:bg-primary',
         className
       )}
       {...props}
     >
       <RadioGroupPrimitive.Indicator className="flex items-center justify-center">
-        <Check className="h-3.5 w-3.5 fill-primary" />
+        <Check className="h-3.5 w-3.5 fill-primary-foreground" />
       </RadioGroupPrimitive.Indicator>
     </RadioGroupPrimitive.Item>
   )
