83bcc25599f6641552cdb2e3387fc2517fb58033
uplusion23 <uplusion23@gmail.com>
2024-12-05 11:01:39 -0600

    fix: resolve build errors
    chore: update dependencies

--- apps/www/next.config.mjs
+++ apps/www/next.config.mjs
@@ -4,7 +4,9 @@ import { createContentlayerPlugin } from "next-contentlayer2";
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+  experimental: {
+    esmExternals: true,
+  },
   images: {
     remotePatterns: [
       {
